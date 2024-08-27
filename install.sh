69604a9d4d593788a679270a538dde2499c70550
NaskiAscii <160227029+NaskiAscii@users.noreply.github.com>
2024-08-27 17:49:00 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
+sudo pip3 install numpy
+sudo pip3 install Pillow
+sudo pip3 install ST7789
